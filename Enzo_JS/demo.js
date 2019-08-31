f4d78092a91383db7416a184aad1a0ab49b53706
crescenzo77 <crescenzo@gmail.com>
2019-08-31 09:57:09 -0500

    updating some json data to include US regions

--- Enzo_JS/demo.js
+++ Enzo_JS/demo.js
@@ -6,3 +6,11 @@ d3.json(urlStates).then(function(dataStates) {
 });
 
 
+const apiRegions = "https://api.usa.gov/crime/fbi/sapi/api/regions?API_KEY=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv";
+
+// Fetch the JSON data and console log it
+d3.json(apiRegions).then(function(regions) {
+  console.log(regions);
+});
+
+
