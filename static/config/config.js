84bea4ee2dd62af9de2fc4227e45a95c318db971
Carol Pietro <carol.m.pietro@gmail.com>
2019-09-19 20:00:19 -0400

    latest TEST

--- static/config/config.js
+++ static/config/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiYmdyYWNlMyIsImEiOiJjanpwbHpsMmwwY2V6M2hvYmV6YWo4N2tzIn0.7g27Dc_Kze7NNR3y76Z3xQ";
+const API_KEY = "pk.eyJ1IjoiaXZhbGVudGlrIiwiYSI6ImNqenBtdzFrdzBybXgzaHJxdDFkeWszc2UifQ.AlqZJKfxfzAP5EPpgxUVVA";
